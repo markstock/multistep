1c1216cfde9be03c990e839ba46081e00a172968
Mark Stock <markjstock@gmail.com>
2022-03-28 15:44:16 -0400

    Moved out multi-step integrators to their own file

---
+++ src/MultistepIntegrator.hpp
@@ -0,0 +1,290 @@
+/*
+ * MultistepIntegrator.hpp - Multi-step forward integrator classes (using history)
+ *
+ * Copyright 2016,22 Mark J. Stock, markjstock@gmail.com
+ */
+
+#pragma once
+
+#include "DynamicState.hpp"
+#include "DynamicalSystem.hpp"
+#include "ForwardIntegrator.hpp"
+#include "MultistageIntegrator.hpp"
+
+#include <cstdint>
+#include <iostream>
+
+
+/*
+ * Multi-step forward integrator class (saves previous solutions)
+ */
+template <class T>
+class MultistepIntegrator : public ForwardIntegrator<T> {
+public:
+  MultistepIntegrator (const int32_t _nsteps, DynamicalSystem<T>& _system, const int32_t _level, const double _dt) :
+    ForwardIntegrator<T>(_system, _nsteps, _level)
+  {
+    // zero state set in parent constructor
+    // set the previous states here
+    RK4<T> r(this->g,0);
+    //std::cout << "in MultistepIntegrator::MultistepIntegrator " << r.getPosition().segment(0,4).transpose() << std::endl;
+    for (int32_t istep=1; istep<_nsteps; ++istep) {
+      for (int32_t i=0; i<100; ++i) r.stepForward(-0.01 * _dt);
+      this->s[istep].step = -istep;
+      for (int32_t deriv=0; deriv<this->g.getNumDerivs(); deriv++) {
+        this->s[istep].x[deriv] = r.getDeriv(deriv);
+      }
+      this->s[istep].x[this->g.getNumDerivs()] = this->g.getHighestDeriv(this->s[istep].x[0]);
+    }
+  }
+};
+
+
+/*
+ * Adams-Bashforth plus Adams-Moulton
+ */
+template <class T>
+class AB2 : public MultistepIntegrator<T> {
+public:
+  AB2 (DynamicalSystem<T>& _system, const int32_t _level, const double _dt) :
+    MultistepIntegrator<T>(2, _system, _level, _dt)
+  {}
+  
+  // always takes a position and velocity and turns it into a new position and velocity
+  void stepForward (const double _dt) {
+    // assert that we have at least two states in the history
+    //static_assert(s.size() >= 2, "State vector does not have enough entries");
+    // assert that this dt matches the previous dt
+    const int32_t numDerivs = this->g.getNumDerivs();
+
+    // ask the system to find its new highest-level derivative
+    this->s[0].x[numDerivs] = this->g.getHighestDeriv(this->s[0].x[0]);
+
+    // add a new state to the head
+    DynamicState<T> newHead = this->s[0].stepHelper();
+    this->s.insert(this->s.begin(), newHead);
+
+    // perform forward integration: AB2 for first one down, AM2 for all others
+    this->s[0].x[numDerivs-1] = this->s[1].x[numDerivs-1] + 0.5 * _dt * (3.0*this->s[1].x[numDerivs] - this->s[2].x[numDerivs]);
+    for (int32_t deriv=numDerivs-1; deriv>0; deriv--) {
+      this->s[0].x[deriv-1] += 0.5 * _dt * (this->s[0].x[deriv] + this->s[1].x[deriv]);
+    }
+
+    // perform forward integration: AB2 for velocity, AM2 for position
+    //this->s[0].x[1] = this->s[1].x[1] + 0.5 * _dt * (3.0*this->s[1].x[2] - this->s[2].x[2]);
+    //this->s[0].x[0] = this->s[1].x[0] + 0.5 * _dt * (this->s[0].x[1] + this->s[1].x[1]);
+
+    // get rid of oldest state
+    this->s.pop_back();
+  }
+};
+
+
+/*
+ * Adams-Bashforth plus Adams-Moulton, all 4th order
+ */
+template <class T>
+class AB4 : public MultistepIntegrator<T> {
+public:
+  AB4 (DynamicalSystem<T>& _system, const int32_t _level, const double _dt) :
+    MultistepIntegrator<T>(4, _system, _level, _dt)
+  {}
+
+  // always takes a position and velocity and turns it into a new position and velocity
+  void stepForward (const double _dt) {
+    const int32_t nd = this->g.getNumDerivs();
+
+    this->s[0].x[nd] = this->g.getHighestDeriv(this->s[0].x[0]);
+
+    // add a new one to the head
+    DynamicState<T> newHead = this->s[0].stepHelper();
+    this->s.insert(this->s.begin(), newHead);
+
+    // perform forward integration: AB4 for first, AM4 for all lower-order derivatives
+    this->s[0].x[nd-1] += _dt * (55.0*this->s[1].x[nd] - 59.0*this->s[2].x[nd] + 37.0*this->s[3].x[nd] - 9.0*this->s[4].x[nd]) / 24.0;
+    for (int32_t deriv=nd-1; deriv>0; deriv--) {
+      this->s[0].x[deriv-1] += _dt * (9.0*this->s[0].x[deriv] + 19.0*this->s[1].x[deriv] - 5.0*this->s[2].x[deriv] + this->s[3].x[deriv]) / 24.0;
+    }
+
+    // perform forward integration: AB4 for velocity, AM4 for position
+    //this->s[0].x[1] = this->s[1].x[1] +  _dt * (55.0*this->s[1].x[2] - 59.0*this->s[2].x[2] + 37.0*this->s[3].x[2] - 9.0*this->s[4].x[2]) / 24.0;
+    //this->s[0].x[0] = this->s[1].x[0] +  _dt * (9.0*this->s[0].x[1] + 19.0*this->s[1].x[1] - 5.0*this->s[2].x[1] + this->s[3].x[1]) / 24.0;
+
+    // get rid of oldest
+    this->s.pop_back();
+  }
+};
+
+
+/*
+ * Adams-Bashforth plus Adams-Moulton, all 5th order
+ */
+template <class T>
+class AB5 : public MultistepIntegrator<T> {
+public:
+  AB5 (DynamicalSystem<T>& _system, const int32_t _level, const double _dt) :
+    MultistepIntegrator<T>(5, _system, _level, _dt)
+  {}
+
+  // always takes a position and velocity and turns it into a new position and velocity
+  void stepForward (const double _dt) {
+    const int32_t nd = this->g.getNumDerivs();
+
+    this->s[0].x[nd] = this->g.getHighestDeriv(this->s[0].x[0]);
+
+    // add a new one to the head
+    DynamicState<T> newHead = this->s[0].stepHelper();
+    this->s.insert(this->s.begin(), newHead);
+
+    // perform forward integration: AB5 for first, AM5 for all lower-order derivatives
+    this->s[0].x[nd-1] += _dt * (1901.0*this->s[1].x[nd] - 2774.0*this->s[2].x[nd] + 2616.0*this->s[3].x[nd] - 1274.0*this->s[4].x[nd] + 251.0*this->s[5].x[nd]) / 720.0;
+    for (int32_t deriv=nd-1; deriv>0; deriv--) {
+      this->s[0].x[deriv-1] += _dt * (251.0*this->s[0].x[deriv] + 646.0*this->s[1].x[deriv] - 264.0*this->s[2].x[deriv] + 106.0*this->s[3].x[deriv] - 19.0*this->s[4].x[deriv]) / 720.0;
+    }
+
+    // perform forward integration: AB5 for velocity, AM5 for position
+    //this->s[0].x[1] = this->s[1].x[1] +  _dt * (1901.0*this->s[1].x[2] - 2774.0*this->s[2].x[2] + 2616.0*this->s[3].x[2] - 1274.0*this->s[4].x[2] + 251.0*this->s[5].x[2]) / 720.0;
+    //this->s[0].x[0] = this->s[1].x[0] +  _dt * (251.0*this->s[0].x[1] + 646.0*this->s[1].x[1] - 264.0*this->s[2].x[1] + 106.0*this->s[3].x[1] - 19.0*this->s[4].x[1]) / 720.0;
+
+    // get rid of oldest
+    this->s.pop_back();
+  }
+};
+
+
+/*
+ * Standard Verlet (non-velocity) integrator
+ */
+template <class T>
+class Verlet : public MultistepIntegrator<T> {
+public:
+  Verlet (AccelerationSystem<T>& _system, const int32_t _level, const double _dt) :
+    MultistepIntegrator<T>(2, _system, _level, _dt)
+  {}
+  
+  void stepForward (const double _dt) {
+    this->s[0].x[2] = this->g.getHighestDeriv(this->s[0].x[0]);
+
+    // add a new one to the head
+    DynamicState<T> newHead(this->g.getNumDerivs(), this->s[0].level, this->s[0].step++);
+    this->s.insert(this->s.begin(), newHead);
+
+    // perform forward integration
+    this->s[0].x[0] = 2.0*this->s[1].x[0] - this->s[2].x[0] +  _dt*_dt*this->s[1].x[2];
+
+    // get rid of oldest
+    this->s.pop_back();
+  }
+};
+
+
+/*
+ * New integrator - uses more history, but still no velocities
+ *
+ * This is effectively performing Richardson extrapolation on the standard Verlet!
+ */
+template <class T>
+class RichardsonVerlet : public MultistepIntegrator<T> {
+public:
+  RichardsonVerlet (AccelerationSystem<T>& _system, const int32_t _level, const double _dt) :
+    MultistepIntegrator<T>(4, _system, _level, _dt)
+  {}
+
+  void stepForward (const double _dt) {
+    this->s[0].x[2] = this->g.getHighestDeriv(this->s[0].x[0]);
+
+    // add a new one to the head
+    DynamicState<T> newHead(this->g.getNumDerivs(), this->s[0].level, this->s[0].step++);
+    this->s.insert(this->s.begin(), newHead);
+
+    // perform forward integration
+    // note precision problems that will arise when positions are subtracted!
+    // maybe the velocity version will be more precise?
+    // just adding the parentheses increases accuracy!
+    this->s[0].x[0] = this->s[3].x[0] + ((this->s[1].x[0] - this->s[4].x[0])
+             + 0.25*_dt*_dt* ( 5.0*this->s[1].x[2]
+                              +2.0*this->s[2].x[2]
+                              +5.0*this->s[3].x[2]));
+
+    // get rid of oldest
+    this->s.pop_back();
+  }
+};
+
+
+/*
+ * Hamming - pg 416
+ *
+ * A predictor using only accelerations and positions
+ */
+template <class T>
+class Hamming416 : public MultistepIntegrator<T> {
+public:
+  Hamming416 (AccelerationSystem<T>& _system, const int32_t _level, const double _dt) :
+    MultistepIntegrator<T>(4, _system, _level, _dt)
+  {}
+  
+  void stepForward (const double _dt) {
+    this->s[0].x[2] = this->g.getHighestDeriv(this->s[0].x[0]);
+
+    // add a new one to the head
+    DynamicState<T> newHead(this->g.getNumDerivs(), this->s[0].level, this->s[0].step++);
+    this->s.insert(this->s.begin(), newHead);
+
+    // perform forward integration
+    this->s[0].x[0] = 2.0*this->s[2].x[0] - this->s[4].x[0]
+             + (4.0*_dt*_dt/3.0) * ( this->s[1].x[2]
+                                    +this->s[2].x[2]
+                                    +this->s[3].x[2]);
+
+    // get rid of oldest
+    this->s.pop_back();
+  }
+};
+
+
+/*
+ * Hamming - pg 418
+ *
+ * A predictor using accelerations, velocities, and positions
+ */
+template <class T>
+class Hamming418 : public MultistepIntegrator<T> {
+public:
+  Hamming418 (AccelerationSystem<T>& _system, const int32_t _level, const double _dt) :
+    MultistepIntegrator<T>(2, _system, _level, _dt)
+  {}
+  
+  void stepForward (const double _dt) {
+    this->s[0].x[2] = this->g.getHighestDeriv(this->s[0].x[0]);
+
+    // add a new one to the head
+    DynamicState<T> newHead(this->g.getNumDerivs(), this->s[0].level, this->s[0].step++);
+    this->s.insert(this->s.begin(), newHead);
+
+    // perform forward integration
+    // first, use Adams Moulton to find the new velocity
+    // NEED SOMETHING BETTER HERE!
+    //this->s[1].x[1] = this->s[2].x[1]
+    //         + (_dt/2.0)      * (     this->s[1].x[2] +     this->s[2].x[2]);
+    // then use the Hamming method to find the position
+    this->s[0].x[0] = this->s[1].x[0]
+             + (_dt/2.0)      * (    -this->s[1].x[1] + 3.0*this->s[2].x[1])
+             + (_dt*_dt/12.0) * (17.0*this->s[1].x[2] + 7.0*this->s[2].x[2]);
+    // this is horrible!
+    this->s[0].x[1] = (384.0/_dt)    * (     this->s[2].x[0] - this->s[1].x[0])
+             + (1.0)          * (312.*this->s[1].x[1] + 73.*this->s[2].x[1])
+             + (-1.0*_dt)     * (110.*this->s[1].x[2] + 8.0*this->s[2].x[2]);
+
+    // get rid of oldest
+    this->s.pop_back();
+  }
+};
+
+
+/*
+ * Try Bulirsch-Stoer algorithm
+ *
+ * https://en.wikipedia.org/wiki/Bulirsch-Stoer_algorithm
+*/
+
--- src/multistep.cpp
+++ src/multistep.cpp
@@ -10,6 +10,7 @@
 #include "NBodyGrav3D.hpp"
 #include "ForwardIntegrator.hpp"
 #include "MultistageIntegrator.hpp"
+#include "MultistepIntegrator.hpp"
 
 #include <Eigen/Dense>
 
@@ -18,6 +19,9 @@
 #include <vector>
 #include <cmath>
 
+
+// Perform Richardson Extrapolation by creating multiple temporal resolution levels automatically?
+// or is that Bulirsch-Stoer?
 
 /*
  * Control class for Richardson Extrapolation and globally adaptive time stepping
@@ -33,274 +37,6 @@ private:
 };
 
 
-/*
- * Multi-step forward integrator class (saves previous solutions)
- */
-class MultistepIntegrator : public ForwardIntegrator<Eigen::ArrayXd> {
-public:
-  MultistepIntegrator (const int32_t _nsteps, DynamicalSystem<Eigen::ArrayXd>& _system, const int32_t _level, const double _dt) :
-    ForwardIntegrator<Eigen::ArrayXd>(_system, _nsteps, _level)
-  {
-    // zero state set in parent constructor
-    // set the previous states here
-    RK4<Eigen::ArrayXd> r(g,0);
-    //std::cout << "in MultistepIntegrator::MultistepIntegrator " << r.getPosition().segment(0,4).transpose() << std::endl;
-    for (int32_t istep=1; istep<_nsteps; ++istep) {
-      for (int32_t i=0; i<100; ++i) r.stepForward(-0.01 * _dt);
-      s[istep].step = -istep;
-      for (int32_t deriv=0; deriv<g.getNumDerivs(); deriv++) {
-        s[istep].x[deriv] = r.getDeriv(deriv);
-      }
-      s[istep].x[g.getNumDerivs()] = g.getHighestDeriv(s[istep].x[0]);
-    }
-  }
-};
-
-
-/*
- * Adams-Bashforth plus Adams-Moulton
- */
-class AB2 : public MultistepIntegrator {
-public:
-  AB2 (DynamicalSystem<Eigen::ArrayXd>& _system, const int32_t _level, const double _dt) :
-    MultistepIntegrator(2, _system, _level, _dt)
-  {}
-  
-  // always takes a position and velocity and turns it into a new position and velocity
-  void stepForward (const double _dt) {
-    // assert that we have at least two states in the history
-    //static_assert(s.size() >= 2, "State vector does not have enough entries");
-    // assert that this dt matches the previous dt
-    const int32_t numDerivs = g.getNumDerivs();
-
-    // ask the system to find its new highest-level derivative
-    s[0].x[numDerivs] = g.getHighestDeriv(s[0].x[0]);
-
-    // add a new state to the head
-    DynamicState<Eigen::ArrayXd> newHead = s[0].stepHelper();
-    s.insert(s.begin(), newHead);
-
-    // perform forward integration: AB2 for first one down, AM2 for all others
-    s[0].x[numDerivs-1] = s[1].x[numDerivs-1] + 0.5 * _dt * (3.0*s[1].x[numDerivs] - s[2].x[numDerivs]);
-    for (int32_t deriv=numDerivs-1; deriv>0; deriv--) {
-      s[0].x[deriv-1] += 0.5 * _dt * (s[0].x[deriv] + s[1].x[deriv]);
-    }
-
-    // perform forward integration: AB2 for velocity, AM2 for position
-    //s[0].x[1] = s[1].x[1] + 0.5 * _dt * (3.0*s[1].x[2] - s[2].x[2]);
-    //s[0].x[0] = s[1].x[0] + 0.5 * _dt * (s[0].x[1] + s[1].x[1]);
-
-    // get rid of oldest state
-    s.pop_back();
-  }
-};
-
-
-/*
- * Adams-Bashforth plus Adams-Moulton, all 4th order
- */
-class AB4 : public MultistepIntegrator {
-public:
-  AB4 (DynamicalSystem<Eigen::ArrayXd>& _system, const int32_t _level, const double _dt) :
-    MultistepIntegrator(4, _system, _level, _dt)
-  {}
-
-  // always takes a position and velocity and turns it into a new position and velocity
-  void stepForward (const double _dt) {
-    const int32_t nd = g.getNumDerivs();
-
-    s[0].x[nd] = g.getHighestDeriv(s[0].x[0]);
-
-    // add a new one to the head
-    DynamicState<Eigen::ArrayXd> newHead = s[0].stepHelper();
-    s.insert(s.begin(), newHead);
-
-    // perform forward integration: AB4 for first, AM4 for all lower-order derivatives
-    s[0].x[nd-1] += _dt * (55.0*s[1].x[nd] - 59.0*s[2].x[nd] + 37.0*s[3].x[nd] - 9.0*s[4].x[nd]) / 24.0;
-    for (int32_t deriv=nd-1; deriv>0; deriv--) {
-      s[0].x[deriv-1] += _dt * (9.0*s[0].x[deriv] + 19.0*s[1].x[deriv] - 5.0*s[2].x[deriv] + s[3].x[deriv]) / 24.0;
-    }
-
-    // perform forward integration: AB4 for velocity, AM4 for position
-    //s[0].x[1] = s[1].x[1] +  _dt * (55.0*s[1].x[2] - 59.0*s[2].x[2] + 37.0*s[3].x[2] - 9.0*s[4].x[2]) / 24.0;
-    //s[0].x[0] = s[1].x[0] +  _dt * (9.0*s[0].x[1] + 19.0*s[1].x[1] - 5.0*s[2].x[1] + s[3].x[1]) / 24.0;
-
-    // get rid of oldest
-    s.pop_back();
-  }
-};
-
-
-/*
- * Adams-Bashforth plus Adams-Moulton, all 5th order
- */
-class AB5 : public MultistepIntegrator {
-public:
-  AB5 (DynamicalSystem<Eigen::ArrayXd>& _system, const int32_t _level, const double _dt) :
-    MultistepIntegrator(5, _system, _level, _dt)
-  {}
-
-  // always takes a position and velocity and turns it into a new position and velocity
-  void stepForward (const double _dt) {
-    const int32_t nd = g.getNumDerivs();
-
-    s[0].x[nd] = g.getHighestDeriv(s[0].x[0]);
-
-    // add a new one to the head
-    DynamicState<Eigen::ArrayXd> newHead = s[0].stepHelper();
-    s.insert(s.begin(), newHead);
-
-    // perform forward integration: AB5 for first, AM5 for all lower-order derivatives
-    s[0].x[nd-1] += _dt * (1901.0*s[1].x[nd] - 2774.0*s[2].x[nd] + 2616.0*s[3].x[nd] - 1274.0*s[4].x[nd] + 251.0*s[5].x[nd]) / 720.0;
-    for (int32_t deriv=nd-1; deriv>0; deriv--) {
-      s[0].x[deriv-1] += _dt * (251.0*s[0].x[deriv] + 646.0*s[1].x[deriv] - 264.0*s[2].x[deriv] + 106.0*s[3].x[deriv] - 19.0*s[4].x[deriv]) / 720.0;
-    }
-
-    // perform forward integration: AB5 for velocity, AM5 for position
-    //s[0].x[1] = s[1].x[1] +  _dt * (1901.0*s[1].x[2] - 2774.0*s[2].x[2] + 2616.0*s[3].x[2] - 1274.0*s[4].x[2] + 251.0*s[5].x[2]) / 720.0;
-    //s[0].x[0] = s[1].x[0] +  _dt * (251.0*s[0].x[1] + 646.0*s[1].x[1] - 264.0*s[2].x[1] + 106.0*s[3].x[1] - 19.0*s[4].x[1]) / 720.0;
-
-    // get rid of oldest
-    s.pop_back();
-  }
-};
-
-
-/*
- * Standard Verlet (non-velocity) integrator
- */
-class Verlet : public MultistepIntegrator {
-public:
-  Verlet (AccelerationSystem<Eigen::ArrayXd>& _system, const int32_t _level, const double _dt) :
-    MultistepIntegrator(2, _system, _level, _dt)
-  {}
-  
-  void stepForward (const double _dt) {
-    s[0].x[2] = g.getHighestDeriv(s[0].x[0]);
-
-    // add a new one to the head
-    DynamicState<Eigen::ArrayXd> newHead(g.getNumDerivs(), s[0].level, s[0].step++);
-    s.insert(s.begin(), newHead);
-
-    // perform forward integration
-    s[0].x[0] = 2.0*s[1].x[0] - s[2].x[0] +  _dt*_dt*s[1].x[2];
-
-    // get rid of oldest
-    s.pop_back();
-  }
-};
-
-
-/*
- * New integrator - uses more history, but still no velocities
- *
- * This is effectively performing Richardson extrapolation on the standard Verlet!
- */
-class RichardsonVerlet : public MultistepIntegrator {
-public:
-  RichardsonVerlet (AccelerationSystem<Eigen::ArrayXd>& _system, const int32_t _level, const double _dt) :
-    MultistepIntegrator(4, _system, _level, _dt)
-  {}
-
-  void stepForward (const double _dt) {
-    s[0].x[2] = g.getHighestDeriv(s[0].x[0]);
-
-    // add a new one to the head
-    DynamicState<Eigen::ArrayXd> newHead(g.getNumDerivs(), s[0].level, s[0].step++);
-    s.insert(s.begin(), newHead);
-
-    // perform forward integration
-    // note precision problems that will arise when positions are subtracted!
-    // maybe the velocity version will be more precise?
-    // just adding the parentheses increases accuracy!
-    s[0].x[0] = s[3].x[0] + ((s[1].x[0] - s[4].x[0])
-             + 0.25*_dt*_dt* ( 5.0*s[1].x[2]
-                              +2.0*s[2].x[2]
-                              +5.0*s[3].x[2]));
-
-    // get rid of oldest
-    s.pop_back();
-  }
-};
-
-
-/*
- * Hamming - pg 416
- *
- * A predictor using only accelerations and positions
- */
-class Hamming416 : public MultistepIntegrator {
-public:
-  Hamming416 (AccelerationSystem<Eigen::ArrayXd>& _system, const int32_t _level, const double _dt) :
-    MultistepIntegrator(4, _system, _level, _dt)
-  {}
-  
-  void stepForward (const double _dt) {
-    s[0].x[2] = g.getHighestDeriv(s[0].x[0]);
-
-    // add a new one to the head
-    DynamicState<Eigen::ArrayXd> newHead(g.getNumDerivs(), s[0].level, s[0].step++);
-    s.insert(s.begin(), newHead);
-
-    // perform forward integration
-    s[0].x[0] = 2.0*s[2].x[0] - s[4].x[0]
-             + (4.0*_dt*_dt/3.0) * ( s[1].x[2]
-                                    +s[2].x[2]
-                                    +s[3].x[2]);
-
-    // get rid of oldest
-    s.pop_back();
-  }
-};
-
-
-/*
- * Hamming - pg 418
- *
- * A predictor using accelerations, velocities, and positions
- */
-class Hamming418 : public MultistepIntegrator {
-public:
-  Hamming418 (AccelerationSystem<Eigen::ArrayXd>& _system, const int32_t _level, const double _dt) :
-    MultistepIntegrator(2, _system, _level, _dt)
-  {}
-  
-  void stepForward (const double _dt) {
-    s[0].x[2] = g.getHighestDeriv(s[0].x[0]);
-
-    // add a new one to the head
-    DynamicState<Eigen::ArrayXd> newHead(g.getNumDerivs(), s[0].level, s[0].step++);
-    s.insert(s.begin(), newHead);
-
-    // perform forward integration
-    // first, use Adams Moulton to find the new velocity
-    // NEED SOMETHING BETTER HERE!
-    //s[1].x[1] = s[2].x[1]
-    //         + (_dt/2.0)      * (     s[1].x[2] +     s[2].x[2]);
-    // then use the Hamming method to find the position
-    s[0].x[0] = s[1].x[0]
-             + (_dt/2.0)      * (    -s[1].x[1] + 3.0*s[2].x[1])
-             + (_dt*_dt/12.0) * (17.0*s[1].x[2] + 7.0*s[2].x[2]);
-    // this is horrible!
-    s[0].x[1] = (384.0/_dt)    * (     s[2].x[0] - s[1].x[0])
-             + (1.0)          * (312.*s[1].x[1] + 73.*s[2].x[1])
-             + (-1.0*_dt)     * (110.*s[1].x[2] + 8.0*s[2].x[2]);
-
-    // get rid of oldest
-    s.pop_back();
-  }
-};
-
-
-/*
- * Try Bulirsch-Stoer algorithm
- *
- * https://en.wikipedia.org/wiki/Bulirsch-Stoer_algorithm
-*/
-
-
-// Perform Richardson Extrapolation by creating multiple temporal resolution levels automatically?
-
 
 // Create a system and an integrator
 int main () {
@@ -313,7 +49,7 @@ int main () {
   double time = 0.0;
   int32_t maxSteps = 10000;
   double dt = 10.0 / maxSteps;
-  AB5 exact(s,0,dt);
+  AB5<Eigen::ArrayXd> exact(s,0,dt);
   std::cout << "'Exact' solution is from running " << maxSteps << " steps of AB5 at dt= " << dt << std::endl;
   for (int32_t i=0; i<maxSteps; ++i) {
     exact.stepForward(dt);
@@ -343,14 +79,14 @@ int main () {
     // initialize integrators
     Euler<Eigen::ArrayXd> e(s,0);
     RK2<Eigen::ArrayXd> r2(s,0);
-    AB2 a(s,0,dt);
-    Verlet ve(s,0,dt);
+    AB2<Eigen::ArrayXd> a(s,0,dt);
+    Verlet<Eigen::ArrayXd> ve(s,0,dt);
     RK4<Eigen::ArrayXd> r(s,0);
-    AB4 ab(s,0,dt);
-    RichardsonVerlet rv(s,0,dt);
-    Hamming416 h6(s,0,dt);
-    Hamming418 h8(s,0,dt);
-    AB5 abb(s,0,dt);
+    AB4<Eigen::ArrayXd> ab(s,0,dt);
+    RichardsonVerlet<Eigen::ArrayXd> rv(s,0,dt);
+    Hamming416<Eigen::ArrayXd> h6(s,0,dt);
+    Hamming418<Eigen::ArrayXd> h8(s,0,dt);
+    AB5<Eigen::ArrayXd> abb(s,0,dt);
 
     for (int32_t i=0; i<maxSteps; ++i) {
 
