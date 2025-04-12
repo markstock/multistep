4f881fdccdc31926b3e96afa8f833ccc54e52d95
Mark Stock <markjstock@gmail.com>
2025-04-12 16:02:17 -0400

    Program handles CL args like -ab -rk and -all to select integrators

--- src/multistep.cpp
+++ src/multistep.cpp
@@ -39,10 +39,71 @@
 // Eigen::Array< __float128, Dynamic, 1 >.
 
 // Create a system and an integrator
-int main () {
+int main (int argc, char *argv[]) {
 
-  const bool dumpevery = false;
-  const bool doHamming = false;
+  constexpr int32_t numInteg = 16;
+  std::string integ[numInteg] = {
+    "Euler",	// 0  Euler
+    "RK2-R",	// 1  RK2 Ralston
+    "RK2-H",	// 2  RK2 Heun
+    "AB2",		// 3  AB2
+    "Verlet",	// 4  Verlet
+    "RK3-R",	// 5  RK3 Ralston
+    "RK3-H",	// 6  RK3 Heun
+    "RK3-K",	// 7  RK3 Kutta
+    "AB3",		// 8  AB3+AM3
+    "RK4",		// 9  RK4 Classic
+    "RK4-TE",	// 10 RK4 3/8ths rule
+    "AB4",		// 11 AB4+AM4
+    "RichVer",	// 12 Richardson-Verlet
+    "Ham416",	// 13 Hamming416
+    "Ham418",	// 14 Hamming418
+    "AB5"		// AB5+AM5
+  };
+
+  bool dumpevery = false;
+  bool doRK = false;
+  bool doAB = false;
+  bool doHam = false;
+  bool doVer = false;
+  bool doInteg[numInteg] = {false};
+
+  // parse command-line arguments
+  // input file can appear anywhere
+  // processing options are performed in the order they appear
+  //(void) strcpy(progname,argv[0]);
+  //if (argc < 2) (void) Usage(progname,0);
+  for (int i=1; i<argc; i++) {
+    if (strncmp(argv[i], "-",1) == 0) {
+      if (strncmp(argv[i], "-all", 4) == 0) {
+        doInteg[0] = true;
+        doRK = true;
+        doAB = true;
+        doVer = true;
+        doHam = true;
+      } else if (strncmp(argv[i], "-rk2", 4) == 0) {
+        doInteg[1] = true;
+        doInteg[2] = true;
+      } else if (strncmp(argv[i], "-rk3", 4) == 0) {
+        doInteg[5] = true;
+        doInteg[6] = true;
+        doInteg[7] = true;
+      } else if (strncmp(argv[i], "-rk4", 4) == 0) {
+        doInteg[9] = true;
+        doInteg[10] = true;
+      } else if (strncmp(argv[i], "-rk", 3) == 0) {
+        doRK = true;
+      } else if (strncmp(argv[i], "-ab", 4) == 0) {
+        doAB = true;
+      } else if (strncmp(argv[i], "-ver", 4) == 0) {
+        doVer = true;
+      } else if (strncmp(argv[i], "-ham", 4) == 0) {
+        doHam = true;
+      } else {
+        // nothing?
+      }
+    }
+  }
 
   // define the dynamical system
   //VelocitySine s(10.0/9.25);
@@ -52,10 +113,51 @@ int main () {
   //AccelerationSine s(40.0);
   //SpringMass s(100,10.0/9.25);
   //LennardJones s(100,1.0,0.02);
-  //NBodyGrav3D s(5);
+  NBodyGrav3D s(5);
   //NBodyGrav3D s(7);
   //NBodyVort2D s(32);
-  Projectiles3D s(64);
+  //Projectiles3D s(64);
+
+  // adjust available integrators based on dynamical system
+  if (s.hasDamping() and doVer) {
+    std::cout << "System has damping, turning off Verlet integrators\n";
+    doVer = false;
+  }
+  if (doVer and not s.hasAccel()) {
+    std::cout << "System does not use acceleration, turning off Verlet integrators\n";
+    doVer = false;
+  }
+  if (doHam and not s.hasAccel()) {
+    std::cout << "System does not use acceleration, turning off Hamming integrators\n";
+    doHam = false;
+  }
+
+  if (doRK) {
+    doInteg[0] = true;
+    doInteg[1] = true;
+    doInteg[2] = true;
+    doInteg[5] = true;
+    doInteg[6] = true;
+    doInteg[7] = true;
+    doInteg[9] = true;
+    doInteg[10] = true;
+  }
+  if (doAB) {
+    doInteg[0] = true;
+    doInteg[3] = true;
+    doInteg[8] = true;
+    doInteg[11] = true;
+    doInteg[15] = true;
+  }
+  if (doVer) {
+    doInteg[4] = true;
+    doInteg[12] = true;
+  }
+  if (doHam) {
+    doInteg[13] = true;
+    doInteg[14] = true;
+  }
+
 
   // each system has its own end time
   double endtime = s.getEndTime();
@@ -64,25 +166,16 @@ int main () {
   TEMPLATEVAR exact = s.getExact(endtime);
 
   std::cout << "steps\t";
-  std::cout << "Euler\t\t";
-  std::cout << "RK2\t\t";
-  std::cout << "AB2\t\t";
-  if (s.hasAccel() and not s.hasDamping()) std::cout << "Verlet\t\t";
-  std::cout << "RK3\t\t";
-  std::cout << "AB3\t\t";
-  std::cout << "RK4\t\t";
-  std::cout << "AB4\t\t";
-  if (s.hasAccel() and not s.hasDamping()) std::cout << "Verlet4\t\t";
-  if (s.hasAccel() and doHamming) std::cout << "Ham416\t\t";
-  if (s.hasAccel() and doHamming) std::cout << "Ham418\t\t";
-  std::cout << "AB5";
+  for (int32_t i=0; i<numInteg; ++i) {
+    if (doInteg[i]) std::cout << integ[i] << "\t\t";
+  }
   std::cout << std::endl;
 
   // set precision for error output
   std::cout << std::setprecision(8);
 
   // integrate using the various methods (always divisible by 12, though!)
-  // ensure that maxsteps is divisible by 12 (to acommodate RK 2,3,4)
+  // ensure that maxsteps is divisible by 12 (to accommodate RK 2,3,4)
   //for (int32_t maxSteps = 60; maxSteps < 2000000; maxSteps = 12*(int)(maxSteps*1.3/12.0)) {
   for (int32_t maxSteps = 60; maxSteps < 130000; maxSteps = 12*(int)(maxSteps*1.3/12.0)) {
 
@@ -92,7 +185,7 @@ int main () {
     std::cout << maxSteps;
 
     // initialize integrators
-    {
+    if (doInteg[0]) {
       Euler<TEMPLATEVAR> e(s,0);
       for (int32_t i=0; i<maxSteps; ++i) {
         //std::cout << "\nvalue is " << e.getPosition();
@@ -106,13 +199,12 @@ int main () {
       std::cout << "\t" << e.getError(exact);
     }
 
-    {
-      //RK2Heun<TEMPLATEVAR> r2(s,0);
+    if (doInteg[1]) {
       RK2Ralston<TEMPLATEVAR> r2(s,0);		// Ralston's is better for larger timesteps only
       for (int32_t i=0; i<maxSteps; ++i) {
         if (i%2 == 0) {
           r2.stepForward(2.0*dt);
-          if (dumpevery) std::cout << "rk2 at t " << r2.getTime() << " is " << r2.getPosition() << "\n";
+          if (dumpevery) std::cout << "rk2r at t " << r2.getTime() << " is " << r2.getPosition() << "\n";
         }
       }
       //TEMPLATEVAR r2Solution = r2.getPosition();
@@ -122,7 +214,22 @@ int main () {
       std::cout << "\t" << r2.getError(exact);
     }
 
-    {
+    if (doInteg[2]) {
+      RK2Heun<TEMPLATEVAR> r2(s,0);
+      for (int32_t i=0; i<maxSteps; ++i) {
+        if (i%2 == 0) {
+          r2.stepForward(2.0*dt);
+          if (dumpevery) std::cout << "rk2h at t " << r2.getTime() << " is " << r2.getPosition() << "\n";
+        }
+      }
+      //TEMPLATEVAR r2Solution = r2.getPosition();
+      //std::cout << "  RK2 solution: " << r2Solution << std::endl;
+      //std::cout << "  RK2 solution: " << r2Solution.segment(0,4).transpose() << std::endl;
+      //std::cout << "  Error in RK2 is " << exact.getError(r2Solution) << std::endl;
+      std::cout << "\t" << r2.getError(exact);
+    }
+
+    if (doInteg[3]) {
       AB2<TEMPLATEVAR> a2(s,0,dt);
       for (int32_t i=0; i<maxSteps; ++i) {
         a2.stepForward(dt);
@@ -134,7 +241,7 @@ int main () {
       std::cout << "\t" << a2.getError(exact);
     }
 
-    if (s.hasAccel() and not s.hasDamping()) {
+    if (doInteg[4]) {
       Verlet<TEMPLATEVAR> ve(s,0,dt);
       for (int32_t i=0; i<maxSteps; ++i) {
         ve.stepForward(dt);
@@ -146,9 +253,7 @@ int main () {
       std::cout << "\t" << ve.getError(exact);
     }
 
-    {
-      //RK3Kutta<TEMPLATEVAR> r3(s,0);
-      //RK3Heun<TEMPLATEVAR> r3(s,0);
+    if (doInteg[5]) {
       RK3Ralston<TEMPLATEVAR> r3(s,0);
       for (int32_t i=0; i<maxSteps; ++i) {
         if (i%3 == 0) {
@@ -162,7 +267,35 @@ int main () {
       std::cout << "\t" << r3.getError(exact);
     }
 
-    {
+    if (doInteg[6]) {
+      RK3Heun<TEMPLATEVAR> r3(s,0);
+      for (int32_t i=0; i<maxSteps; ++i) {
+        if (i%3 == 0) {
+          r3.stepForward(3.0*dt);
+          if (dumpevery) std::cout << "rk3 at t " << r3.getTime() << " is " << r3.getPosition() << "\n";
+        }
+      }
+      //TEMPLATEVAR r3Solution = r3.getPosition();
+      //std::cout << "  RK3 solution: " << r3Solution.segment(0,4).transpose() << std::endl;
+      //std::cout << "  Error in RK3 is " << exact.getError(r3Solution) << std::endl;
+      std::cout << "\t" << r3.getError(exact);
+    }
+
+    if (doInteg[7]) {
+      RK3Kutta<TEMPLATEVAR> r3(s,0);
+      for (int32_t i=0; i<maxSteps; ++i) {
+        if (i%3 == 0) {
+          r3.stepForward(3.0*dt);
+          if (dumpevery) std::cout << "rk3 at t " << r3.getTime() << " is " << r3.getPosition() << "\n";
+        }
+      }
+      //TEMPLATEVAR r3Solution = r3.getPosition();
+      //std::cout << "  RK3 solution: " << r3Solution.segment(0,4).transpose() << std::endl;
+      //std::cout << "  Error in RK3 is " << exact.getError(r3Solution) << std::endl;
+      std::cout << "\t" << r3.getError(exact);
+    }
+
+    if (doInteg[8]) {
       AB3<TEMPLATEVAR> a3(s,0,dt);
       for (int32_t i=0; i<maxSteps; ++i) {
         a3.stepForward(dt);
@@ -174,7 +307,7 @@ int main () {
       std::cout << "\t" << a3.getError(exact);
     }
 
-    {
+    if (doInteg[9]) {
       RK4<TEMPLATEVAR> r4(s,0);
       for (int32_t i=0; i<maxSteps; ++i) {
         if (i%4 == 0) {
@@ -188,7 +321,21 @@ int main () {
       std::cout << "\t" << r4.getError(exact);
     }
 
-    {
+    if (doInteg[10]) {
+      RK4<TEMPLATEVAR> r4(s,0);
+      for (int32_t i=0; i<maxSteps; ++i) {
+        if (i%4 == 0) {
+          r4.stepForward(4.0*dt);
+          if (dumpevery) std::cout << "rk4 at t " << r4.getTime() << " is " << r4.getPosition() << "\n";
+        }
+      }
+      //TEMPLATEVAR r4Solution = r4.getPosition();
+      //std::cout << "  RK4 solution: " << r4Solution.segment(0,4).transpose() << std::endl;
+      //std::cout << "  Error in RK4 is " << exact.getError(r4Solution) << std::endl;
+      std::cout << "\t" << r4.getError(exact);
+    }
+
+    if (doInteg[11]) {
       AB4<TEMPLATEVAR> ab(s,0,dt);
       for (int32_t i=0; i<maxSteps; ++i) {
         ab.stepForward(dt);
@@ -200,7 +347,7 @@ int main () {
       std::cout << "\t" << ab.getError(exact);
     }
 
-    if (s.hasAccel() and not s.hasDamping()) {
+    if (doInteg[12]) {
       RichardsonVerlet<TEMPLATEVAR> rv(s,0,dt);
       for (int32_t i=0; i<maxSteps; ++i) {
         rv.stepForward(dt);
@@ -212,7 +359,7 @@ int main () {
       std::cout << "\t" << rv.getError(exact);
     }
 
-    if (s.hasAccel() and doHamming) {
+    if (doInteg[13]) {
       Hamming416<TEMPLATEVAR> h6(s,0,dt);
       for (int32_t i=0; i<maxSteps; ++i) {
         h6.stepForward(dt);
@@ -221,7 +368,7 @@ int main () {
       std::cout << "\t" << h6.getError(exact);
     }
 
-    if (s.hasAccel() and doHamming) {
+    if (doInteg[14]) {
       Hamming418<TEMPLATEVAR> h8(s,0,dt);
       for (int32_t i=0; i<maxSteps; ++i) {
         h8.stepForward(dt);
@@ -230,7 +377,7 @@ int main () {
       std::cout << "\t" << h8.getError(exact);
     }
 
-    {
+    if (doInteg[15]) {
       AB5<TEMPLATEVAR> abb(s,0,dt);
       for (int32_t i=0; i<maxSteps; ++i) {
         abb.stepForward(dt);
